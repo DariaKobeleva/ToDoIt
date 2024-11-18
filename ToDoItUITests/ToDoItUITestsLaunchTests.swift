31594ee156286d5da2bd3481b2bef946438d1100
Kobeleva Daria <daria.kobeleva.dev@gmail.com>
2024-11-18 12:27:25 +0100

    Initial Commit

---
+++ ToDoItUITests/ToDoItUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ToDoItUITestsLaunchTests.swift
+//  ToDoItUITests
+//
+//  Created by Daria Kobeleva on 18.11.2024.
+//
+
+import XCTest
+
+final class ToDoItUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
